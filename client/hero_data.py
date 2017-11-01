0e97235ab3ad16b997479dbe6c5dfdc1325b7640
vesche <austinjackson892@gmail.com>
2017-11-01 14:11:12 +0000

    Add genie into game

--- client/hero_data.py
+++ client/hero_data.py
@@ -9,7 +9,8 @@ hero_dict = {
     '1': 'batman',
     '2': 'santa',
     '3': 'drunk mickey',
-    '4': 'teen satan'
+    '4': 'teen satan',
+    '5': 'geenie'
 }
 
 # 'hero' : [attack, block, throw]
@@ -17,5 +18,6 @@ hero_moves = {
     'batman': ['Batarang', 'Shadow Step', 'Swear To Me'],
     'santa': ['Coal Throw', 'Present Shield', 'Santa Spank'],
     'drunk mickey': ['Fist Bump', 'Bottle Block', 'Drunk Hug'],
-    'teen satan': ['Flame Arc', 'Firewall', 'False Love']
+    'teen satan': ['Flame Arc', 'Firewall', 'False Love'],
+    'geenie': ['Pistol', 'Knight\'s Shield', 'Tornado']
 }
